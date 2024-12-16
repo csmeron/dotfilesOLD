8445bdc0c3f8c2db14105c6504bf677670acf541
csmeron <csmeron@proton.me>
2024-12-16 15:31:58 -0600

    Updated README.md

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -1,43 +1,43 @@
 -- core files needed for optimal functionality
 return {
-    -- plenary for utility
-    "nvim-lua/plenary.nvim",
+	-- plenary for utility
+	"nvim-lua/plenary.nvim",
 
-    -- conform for formatting
-    {
-        "stevearc/conform.nvim",
-        event = "BufWritePre", -- comment for no format on save
-        opts = require "configs.conform",
-    },
+	-- conform for formatting
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre", -- comment for no format on save
+		opts = require("configs.conform"),
+	},
 
-    -- mason for package management + lsp-config for it
-    {
-        "williamboman/mason.nvim",
-        cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
-        opts = {
-            ensure_installed = {
-                "isort",
-                "black",
-                "mypy",
-                "ruff",
-                "pyright",
-                "prettier",
-                "prettierd",
-                "stylua",
-                "debugpy",
-            },
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-        },
-        config = function()
-            require "configs.lspconfig"
-        end,
-    },
-
+	-- mason for package management + lsp-config for it
+	{
+		"williamboman/mason.nvim",
+		cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
+		opts = {
+			ensure_installed = {
+				"isort",
+				"black",
+				"mypy",
+				"ruff",
+				"pyright",
+				"prettier",
+				"prettierd",
+				"stylua",
+				"debugpy",
+				"markdownlint",
+				"vale",
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
 }
-
